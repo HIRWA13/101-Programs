8f0418d07463046d0da826fc53ba308733d74c33
HIRWA Junior <iamhirwejr@gmail.com>
2023-01-05 17:26:15 +0200

    first program

---
+++ 1-print_101.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+  * main - entry function
+  *
+  * first program in my 101-programs collection
+  *
+  *  Return: 0 on success
+  */
+int main()
+{
+	printf("Welcome to my 101-programs collection\n");
+
+	return (0);
+}
